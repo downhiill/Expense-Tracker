92c26182c447e4dd8d76d681c095fa4e84f880ec
make0ver <downh068@gmail.com>
2024-08-24 19:15:51 +0300

    19:15 "Я устал("

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhBYVB3WmFZfVpgd19DZlZRQWYuP1ZhSXxXdk1hXX9ddXJVQ2BdV0I=");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
